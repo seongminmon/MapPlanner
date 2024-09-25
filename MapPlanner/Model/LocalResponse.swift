3bebe2dd8d4bff2f14c9cbce92eb340f8908f1da
seongmin kim <k2417000@naver.com>
2024-09-25 17:05:28 +0900

    장소 검색 API 구현

---
+++ MapPlanner/Model/LocalResponse.swift
@@ -0,0 +1,41 @@
+//
+//  LocalResponse.swift
+//  MapPlanner
+//
+//  Created by 김성민 on 9/25/24.
+//
+
+import Foundation
+
+struct LocalResponse: Decodable {
+    let meta: Meta
+    let documents: [Document]
+}
+
+struct Meta: Decodable {
+    let total_count: Int
+    let pageable_count: Int
+    let is_end: Bool
+    let same_name: SameName
+}
+
+struct SameName: Decodable {
+    let region: [String]
+    let keyword: String
+    let selected_region: String
+}
+
+struct Document: Decodable {
+    let id: String
+    let place_name: String
+    let category_name: String
+    let category_group_code: String
+    let category_group_name: String
+    let phone: String
+    let address_name: String
+    let road_address_name: String
+    let x: String
+    let y: String
+    let place_url: String
+    let distance: String?
+}
---
+++ MapPlanner/NetworkManager.swift
@@ -0,0 +1,45 @@
+//
+//  NetworkManager.swift
+//  MapPlanner
+//
+//  Created by 김성민 on 9/25/24.
+//
+
+import Foundation
+
+final class NetworkManager {
+    static let shared = NetworkManager()
+    
+    // 키워드로 장소 검색하기
+    private let urlString = "https://dapi.kakao.com/v2/local/search/keyword"
+    
+    func callRequest(_ query: String, page: Int = 1) async throws -> LocalResponse {
+        guard var urlComponents = URLComponents(string: urlString) else {
+            throw URLError(.badURL)
+        }
+        
+        // 쿼리 파라미터 추가
+        urlComponents.queryItems = [
+            URLQueryItem(name: "query", value: query),
+            URLQueryItem(name: "page", value: "\(page)")
+        ]
+        
+        guard let url = urlComponents.url else {
+            throw URLError(.badURL)
+        }
+        
+        // 헤더 추가
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.addValue(APIKey.localSearchKey, forHTTPHeaderField: "Authorization")
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        guard let httpResponse = response as? HTTPURLResponse,
+              httpResponse.statusCode == 200 else {
+            throw URLError(.badServerResponse)
+        }
+        
+        let decodedData = try JSONDecoder().decode(LocalResponse.self, from: data)
+        return decodedData
+    }
+}
