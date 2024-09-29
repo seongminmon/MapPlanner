037e3e5470d705704998d9982bc7de0cc3649b14
seongmin kim <k2417000@naver.com>
2024-09-29 20:18:45 +0900

    설정 화면 뷰 구성

--- MapPlanner/View/Profile/SettingView.swift
+++ MapPlanner/View/Profile/SettingView.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct SettingView: View {
+    
+    // TODO: -
+    // 개인 정보 처리 방침
+    // 오픈 소스 라이센스
+    // 버전 정보
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
-        Text("SettingView")
+        List {
+            Text("개인 정보 처리 방침")
+            Text("오픈 소스 라이센스")
+            Text("버전 정보")
+        }
+        // 네비게이션 바
+        .navigationTitle("설정")
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    dismiss()
+                } label: {
+                    Image.leftChevron
+                }
+                .foregroundStyle(Color(.appPrimary))
+            }
+        }
     }
-}
-
-#Preview {
-    SettingView()
 }
