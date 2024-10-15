6b3951f0771984e6f57242786bbde2e86ac51757
seongmin kim <k2417000@naver.com>
2024-10-15 21:47:46 +0900

    네이버 길찾기 연결 메서드 구현

--- MapPlanner/View/Map/MainMapView.swift
+++ MapPlanner/View/Map/MainMapView.swift
@@ -88,6 +88,22 @@ final class Coordinator: NSObject, ObservableObject {
             }
         }
     }
+    
+    // 네이버 길찾기로 연결
+    func traceRoute(lat: Double, lng: Double) {
+        // URL Scheme을 사용하여 네이버맵 앱을 열고 자동차 경로 생성
+        guard let url = URL(string: "nmap://route/car?dlat=\(lat)&dlng=\(lng)&MapPlanner=kr.co.kepco.ElectricCar") else { return }
+        // 앱 스토어 URL을 설정합니다.
+        guard let appStoreURL = URL(string: "http://itunes.apple.com/app/id311867728?mt=8") else { return }
+
+        if UIApplication.shared.canOpenURL(url) {
+            // 네이버맵 앱이 설치되어 있는 경우 앱을 엽니다.
+            UIApplication.shared.open(url)
+        } else {
+            // 네이버맵 앱이 설치되어 있지 않은 경우 앱 스토어로 이동합니다.
+            UIApplication.shared.open(appStoreURL)
+        }
+    }
 }
 
 // MARK: - 위치 권한 관련 메서드
